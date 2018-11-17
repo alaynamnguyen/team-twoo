6e98831832e9db1a5b16371d85589507d30e648d
John Matthew Narofsky <7ytd765789@gmail.com>
2018-11-17 10:26:01 -0600

    Outlining...

--- search.js
+++ search.js
@@ -1,7 +1,14 @@
+var CLUB_DATA = [];
+function loadData() {} //Call sometime after page load, populate CLUB_DATA.
+
+//This will be a callback for search events or something.
+function search(e) {} // -> [club]
+
 function matches(args) {
 	//Search through clubs, somehow.
 	return [{
 		"name" : "Dummy club",
-		"keywords" : ["computer science", "math"]
+		"keywords" : ["computer science", "math"],
+		"schedule" : ["meeting time and date", "meeting time and date", "..."]
 	}];
 }
